b199aee31231cd8e7a7ea33200bc0e54ff3de212
Twesh Lyallpuri <tweshlyallpuri@gmail.com>
2020-09-07 21:50:33 +0530

    Add files via upload
    Adding a Reminder script, which can be scheduled using WIndows Task Scheduler to add simple reminders of opening a particular link provided in the argument of the same

---
+++ Reminder.ps1
@@ -0,0 +1,21 @@
+param ($url) #named parameter for user to pass when calling for reminders
+ 
+if([string]::IsNullOrEmpty($url))
+{
+	$url=https://google.com # default url just to open Edge window with
+}
+# beep thrice before displaying the window
+for($i = 0; $i -lt 3; $i++){[console]::Beep(200,800);Start-Sleep(0.5); };
+
+# Display YesNo MessageBox. Yes opens the link, No does nothing assuming the Reminder is not needed
+Add-Type -AssemblyName PresentationFramework
+$msgBoxInput =[System.Windows.MessageBox]::Show('Would you like to open the Reminder Link?','Reminder','YesNo','Warning')
+
+switch($msgBoxInput) {
+'Yes' {
+start microsoft-edge:$url
+}
+'No' {
+## Do nothing
+}
+}
